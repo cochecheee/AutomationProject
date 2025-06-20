865f3c89083bfc172638591a8542a79d22b7d4b1
tien.blt <buitien747@gmail.com>
2025-06-20 22:13:41 +0700

    generate random mail for testing

--- src/test/java/automation/register/TC_RF_001.java
+++ src/test/java/automation/register/TC_RF_001.java
@@ -1,13 +1,14 @@
 package automation.register;
 
 import java.time.Duration;
+import java.util.Date;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC_RF_001 {
 
-	public static void main(String[] args) {
+	public static void verifyRegisterWithMandatoryFields(String[] args) {
 		// TODO Auto-generated method stub
 		ChromeDriver driver = new ChromeDriver();
 		//		Set up		
@@ -22,13 +23,20 @@ public class TC_RF_001 {
 		
 		driver.findElement(By.id("input-firstname")).sendKeys("Co");
 		driver.findElement(By.id("input-lastname")).sendKeys("cheche");
-		driver.findElement(By.id("input-email")).sendKeys("cochechene@gmail.com");
+		driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
 		driver.findElement(By.id("input-telephone")).sendKeys("0123456789");
 		driver.findElement(By.id("input-password")).sendKeys("123456");
 		driver.findElement(By.id("input-confirm")).sendKeys("123456");
 		driver.findElement(By.name("agree")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
 		
+	}
+	
+	public static String generateNewEmail() {
+		String noSpaceDateString = new Date().toString().replaceAll("\\s", "");
+		String noSpaceAndnoColonsDateString = noSpaceDateString.replaceAll("\\:", "");
+		String emailWithTimeStamp = noSpaceAndnoColonsDateString + "@gmail.com";
+		return emailWithTimeStamp;
 	}
 
 }
