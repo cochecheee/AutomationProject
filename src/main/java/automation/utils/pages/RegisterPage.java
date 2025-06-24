a6c30876d380845f64454b49fa7554b029dc4e03
tien.blt <buitien747@gmail.com>
2025-06-24 14:52:54 +0700

    adding all needed pages for register testing

--- src/main/java/automation/utils/pages/RegisterPage.java
+++ src/main/java/automation/utils/pages/RegisterPage.java
@@ -23,6 +23,43 @@ public class RegisterPage {
 	@FindBy(id="input-lastname")
 	private WebElement lastNameField;
 	public void enterLastName(String lastName) {
-		firstNameField.sendKeys(lastName);
+		lastNameField.sendKeys(lastName);
+	}
+	
+	@FindBy(id="input-email")
+	private WebElement emailField;
+	public void enterEmail(String email) {
+		emailField.sendKeys(email);
+	}
+	
+	@FindBy(id="input-telephone")
+	private WebElement telephoneField;
+	public void enterTelephone(String telephone) {
+		telephoneField.sendKeys(telephone);
+	}
+	
+	@FindBy(id="input-password")
+	private WebElement passwordField;
+	public void enterPassword(String password) {
+		passwordField.sendKeys(password);
+	}
+	
+	@FindBy(id="input-confirm")
+	private WebElement confirmField;
+	public void enterConfirmPassword(String confirmPassword) {
+		confirmField.sendKeys(confirmPassword);
+	}
+	
+	@FindBy(name="agree")
+	private WebElement privacyPolicyCB;
+	public void checkPrivacyPolicy() {
+		privacyPolicyCB.click();
+	}
+
+	@FindBy(xpath = "//input[@value='Continue']")
+	public WebElement continueButton;
+	public AccountSuccessPage clickOnContinueBtn() {
+		continueButton.click();
+		return new AccountSuccessPage(driver);
 	}
 }
--- src/test/java/automation/register/TC_RF_001.java
+++ src/test/java/automation/register/TC_RF_001.java
@@ -1,6 +1,5 @@
 package automation.register;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.testng.Assert;
@@ -10,6 +9,8 @@ import org.testng.annotations.Test;
 
 import automation.base.Base;
 import automation.utils.CommonUtils;
+import automation.utils.pages.AccountPage;
+import automation.utils.pages.AccountSuccessPage;
 import automation.utils.pages.LandingPage;
 import automation.utils.pages.RegisterPage;
 
@@ -17,6 +18,8 @@ public class TC_RF_001 extends Base{
 	private WebDriver driver;
 	private LandingPage landingPage;
 	private RegisterPage registerPage;
+	private AccountSuccessPage accountSuccessPage;
+	private AccountPage accountPage;
 	
 	@BeforeMethod
 	public void setup() {
@@ -41,21 +44,22 @@ public class TC_RF_001 extends Base{
 		
 		registerPage.enterFirstName("Co");
 		registerPage.enterLastName("cheche");
-		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.generateNewEmail());
-		driver.findElement(By.id("input-telephone")).sendKeys("0123456789");
-		driver.findElement(By.id("input-password")).sendKeys("123456");
-		driver.findElement(By.id("input-confirm")).sendKeys("123456");
-		driver.findElement(By.name("agree")).click();
-		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		registerPage.enterEmail(CommonUtils.generateNewEmail());
+		registerPage.enterTelephone("012345604");
+		registerPage.enterPassword("123456");
+		registerPage.enterConfirmPassword("123456");
 		
-		Assert.assertTrue(driver.findElement(By.linkText("Logout")).isDisplayed());
+		registerPage.checkPrivacyPolicy();
+		accountSuccessPage = registerPage.clickOnContinueBtn();
+		
+		Assert.assertTrue(accountSuccessPage.isLogoutDisplayed());
 		
 		String expectedHeading = "Your Account Has Been Created!";
 		
-		Assert.assertEquals(driver.findElement(By.xpath("//div[@id='common-success']//h1")).getText(), expectedHeading);
+		Assert.assertEquals(accountSuccessPage.getPageHeadingText(), expectedHeading);
 		
-		driver.findElement(By.xpath("//a[text()='Continue']")).click();
-		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+		accountPage = accountSuccessPage.clickOnContinue();
+		Assert.assertTrue(accountPage.isAccountPageDisplayed());
 		
 	}
 
